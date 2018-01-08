cf8e6adb163e83008bec408a25ad498009d6cad7
Mark Fay <fay.mark@gmail.com>
2018-01-08 14:12:03 -0500

    added log file

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import urllib2
 import pyglet
+import time
 from pyquery import PyQuery as pq
-from time import sleep
+
+
+def log(msg):
+    with open('./logs/log.txt', 'a') as f:
+        f.write("[%s] %s\n" % (time.strftime("%Y-%m-%d %H:%M"), msg))
 
 url = "https://shop.bitmain.com/antminer_s9_asic_bitcoin_miner.htm"
 selector = ".btn-buy-now"
@@ -10,10 +15,11 @@ response = urllib2.urlopen(url)
 doc = pq(response.read())
 while True:
     if doc(selector).text() == "Coming soon":
-        print("Still not available")
-        sleep(60)
+        log("Still not available")
+        time.sleep(60)
     else:
+        log("On sale!!!")
         while True:
             music = pyglet.resource.media("alert.wav")
             music.play()
-            sleep(10)
+            time.sleep(10)
